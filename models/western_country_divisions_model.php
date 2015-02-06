ed84c346d8053d734c46c016687d83260ed94921
Vincent Wei <vincent@minigui.org>
2015-02-06 11:38:26 +0800

    add license declaration

--- models/western_country_divisions_model.php
+++ models/western_country_divisions_model.php
@@ -1,4 +1,31 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * This file is a part of Open Cloud API Project.
+ *
+ * Open Cloud API project tries to provide free APIs for internet apps
+ * to fetch public structured data (such as country list) or some
+ * common computing services (such as generating QR code).
+ *
+ * For more information, please refer to:
+ *
+ *		http://www.fullstackengineer.net/zh/project/open-cloud-api-zh
+ *		http://www.fullstackengineer.net/en/project/open-cloud-api-en
+ *
+ * Copyright (C) 2015 WEI Yongming
+ * <http://www.fullstackengineer.net/zh/engineer/weiyongming>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 class Western_country_divisions_model extends CI_Model {
 
