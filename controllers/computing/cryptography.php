8fd8b44d04c1b77e9fb6388eb7c3283d375e75d1
Vincent Wei <vincent@minigui.org>
2015-02-09 18:58:44 +0800

    add first computing endpoint: cryptography

---
+++ controllers/computing/cryptography.php
@@ -0,0 +1,96 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * This file is a part of Open Cloud API Project.
+ *
+ * Open Cloud API project tries to provide free APIs for internet apps
+ * to fetch public structured data (such as country list) or some
+ * common computing services (such as generating QR code).
+ *
+ * For more information, please refer to:
+ *
+ *		http://www.fullstackengineer.net/zh/project/open-cloud-api-zh
+ *		http://www.fullstackengineer.net/en/project/open-cloud-api-en
+ *
+ * Copyright (C) 2015 WEI Yongming
+ * <http://www.fullstackengineer.net/zh/engineer/weiyongming>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+class Cryptography extends MY_Controller {
+
+	protected $_endpoint_list = array (
+					'get_hash' => '/computing/cryptography/hash/{token}/{algo}{/param}',
+					'get_hash_hmac' => '/computing/cryptography/hash_hmac/{token}/{algo}/{key}{/param}',
+				);
+
+	public function index() {
+		$this->output->set_content_type('application/json; charset=utf-8');
+		$data['usage'] = $this->_endpoint_list;
+		$this->load->view('usage', $data);
+	}
+
+	public function hash ($token, $algo, $param = '') {
+		$endpoint_name = 'get_hash';
+
+		if (!isset ($token) || !isset ($algo)) {
+			return;
+		}
+
+		if (!in_array ($algo, hash_algos ())) {
+			show_error ('Bad hash algorithm.', 400);
+			return;
+		}
+
+		if (!parent::_check_callback ($data)) {
+			return;
+		}
+
+		if (!parent::_check_token ($token, $endpoint_name, "hash ($algo, $param)")) {
+			return;
+		}
+
+		$this->output->set_content_type('application/json; charset=utf-8');
+		$data['endpoint'] = $this->_endpoint_list[$endpoint_name];
+		$data['message'] = hash ($algo, $param);
+		$this->load->view('message', $data);
+	}
+
+	public function hash_hmac ($token, $algo, $key, $param = '') {
+		$endpoint_name = 'get_hash_hmac';
+
+		if (!isset ($token) || !isset ($algo) || !isset ($key)) {
+			return;
+		}
+
+		if (!in_array ($algo, hash_algos ())) {
+			show_error ('Bad hash algorithm.', 400);
+			return;
+		}
+
+		if (!parent::_check_callback ($data)) {
+			return;
+		}
+
+		if (!parent::_check_token ($token, $endpoint_name, "hash_hmac ($algo, $param, $key)")) {
+			return;
+		}
+
+		$this->output->set_content_type('application/json; charset=utf-8');
+		$data['endpoint'] = $this->_endpoint_list[$endpoint_name];
+		$data['message'] = hash_hmac ($algo, $param, $key);
+		$this->load->view('message', $data);
+	}
+}
+
+/* End of file cryptography.php */
--- controllers/computing/welcome.php
+++ controllers/computing/welcome.php
@@ -28,17 +28,11 @@
  */
 
 class Welcome extends CI_Controller {
-
 	protected $_endpoint_list = array (
-			'installation' => '/install',
-			'access_token' => '/access_token',
-			'whoami' => '/whoami',
-			'echome' => '/echome',
-			'data_in_list' => '/list',
-			'computing' => '/computing',
+			'cryptography' => '/computing/cryptography',
 		);
 
-	public function index() {
+	public function index () {
 		$this->output->set_content_type('application/json; charset=utf-8');
 		$data['usage'] = $this->_endpoint_list;
 		$this->load->view('usage', $data);
@@ -46,4 +40,3 @@ class Welcome extends CI_Controller {
 }
 
 /* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
